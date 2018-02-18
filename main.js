1eaf6d8b5a61024a0b4ce51beab6b27aca41c23e
Pedro Gabriel Gabiatti Guardini <pgabiattiguardini@gmail.com>
2018-02-18 11:21:25 -0300

    canvasDraw v1.2.0 , stylized the initial content

---
+++ main.js
@@ -0,0 +1,33 @@
+function fallBackground(){
+    var background = document.getElementById('fall');
+    background.classList.add('close');
+}
+var width = 900;//getElementById('width').value;
+var height = 500;//getElementById('height').value;
+
+var canvas = document.getElementById("canvas");
+// "width = attribute name"
+canvas.setAttribute("width", width);
+canvas.setAttribute("height", height);
+
+var ctx = canvas.getContext('2d');
+
+var drawing = false;
+
+canvas.onmousedown = function(evt){
+    ctx.moveTo(evt.clientX, evt.clientY);
+    drawing = true;
+}
+canvas.onmouseup = function(){
+    drawing = false;
+}
+canvas.onmousemove = function(evt){
+    if(drawing){
+        ctx.lineTo(evt.clientX, evt.clientY);
+        ctx.stroke();
+    }
+}
+function clean(){
+    ctx.clearRect(0,0,width,height);
+    ctx.beginPath();
+}
