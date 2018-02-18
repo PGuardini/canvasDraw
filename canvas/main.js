c0485b0e7f9007c48006f7e055c9f3b9dbc15d60
Pedro Guardini <32268641+PGuardini@users.noreply.github.com>
2018-02-17 22:18:14 -0200

    canvasDraw v1.0

---
+++ canvas/main.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    
+    var width = 500;//getElementById('width').value;
+    var height = 300;//getElementById('height').value;
+
+    var canvas = document.getElementById("canvas");
+    // "width = attribute name"
+    canvas.setAttribute("width", width);
+    canvas.setAttribute("height", height);
+
+    var ctx = canvas.getContext('2d');
+
+    var drawing = false;
+
+    canvas.onmousedown = function(evt){
+        ctx.moveTo(evt.clientX, evt.clientY);
+        drawing = true;
+    }
+    canvas.onmouseup = function(){
+        drawing = false;
+    }
+    canvas.onmousemove = function(evt){
+        if(drawing){
+            ctx.lineTo(evt.clientX, evt.clientY);
+            ctx.stroke();
+        }
+    }
+}
